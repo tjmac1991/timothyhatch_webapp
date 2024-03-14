64cf97437322d72fc29745a8837da99d94a72dc4
Tim Hatch <timandkaylee@gmail.com>
2024-03-14 09:09:49 -0600

    Added about text and styles

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -22,6 +22,13 @@ export default function About(): ReactElement {
                     );
                 })}
             </Box>
+            <Box component="p">
+                &nbsp;&nbsp;&nbsp;&nbsp;My name is Timothy Hatch, I am a Full Cycle Developer, Gamer, Rubik's cube solver, Golfer, Hockey player, Tech enthusiast (can't stop me from taking apart a phone and seeing what's inside).
+                I have been taking projects from inception to production for almost 9 years now.
+                I've worked across a variety of stacks and technologies, one of my favorites was developing motion code to move a 6-axis robot arm through our 3d world to help automate manufacturing.
+                Along the way I have learned that there is so much to know and learn as a developer and I am never going to know all of it.
+                The things I have found to overcome the knowledge barrier are: always being ready to learn and knowing how to research to find the answer.
+            </Box>
             <PdfViewer file="./hatch_resume_2024.pdf" />
         </Box>
     );
