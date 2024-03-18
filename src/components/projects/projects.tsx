53765fbc4cbf090864dfbc3d204717606a2e3586
Tim Hatch <timandkaylee@gmail.com>
2024-03-18 09:16:53 -0600

    Updated styles for project page

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -7,17 +7,25 @@ import StarFactionsIcon from "../../assets/icons/rsz_star_factions.png";
 export default function Projects(): ReactElement {
     return (
         <Box className="Projects">
-            <Typography variant="h4">School Projects</Typography>
+            <Typography className="Projects__main-title" variant="h4">School Projects</Typography>
             
-            <Typography variant="h5">Javascript Games (made with Perlenspiel)</Typography>
-            <Box component="a" href="/Frogger/game.html">
-                <img src={StealTheCheeseIcon} />
-            </Box>
-            <Box component="a" href="/EtchSketch/game.html">
-                <img src={EtchSketchIcon} />
+            <Typography className="Projects--sub-title" variant="h5">Javascript Games (made with Perlenspiel)</Typography>
+            <Box className="Projects__js-games">
+                <Box className="Projects--js-game">
+                    <span>Steal the Cheese</span>
+                    <Box component="a" href="/Frogger/game.html">
+                        <img src={StealTheCheeseIcon} />
+                    </Box>
+                </Box>
+                <Box className="Projects--js-game">
+                    <span>Etch-a-Sketch</span>
+                    <Box component="a" href="/EtchSketch/game.html">
+                        <img src={EtchSketchIcon} />
+                    </Box>
+                </Box>
             </Box>
             <Box className="Projects__board-game">
-                <Typography variant="h5">Board Game (with printables, designed with a team)</Typography>
+                <Typography className="Projects--sub-title" variant="h5">Board Game (with printables, designed with a team)</Typography>
                 <Box component="a" href="/TeamAnonymoose_Prototype_0114.pdf">
                     <img className="Projects__board-game--image" src={StarFactionsIcon} />
                 </Box>
