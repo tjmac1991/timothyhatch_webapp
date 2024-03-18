69f5600ccade176293cca6091dc0530dd7066a51
Tim Hatch <timandkaylee@gmail.com>
2024-03-17 22:34:42 -0600

    Update about text

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -23,10 +23,10 @@ export default function About(): ReactElement {
                 })}
             </Box>
             <Box component="p">
-                &nbsp;&nbsp;&nbsp;&nbsp;My name is Timothy Hatch, I am a Full Cycle Developer, Gamer, Rubik's cube solver, Golfer, Hockey player, Tech enthusiast (can't stop me from taking apart a phone and seeing what's inside).
-                I have been taking projects from inception to production for almost 9 years now.
-                I've worked across a variety of stacks and technologies, one of my favorites was developing motion code to move a 6-axis robot arm through our 3d world to help automate manufacturing.
-                Along the way I have learned that there is so much to know and learn as a developer and I am never going to know all of it.
+                &nbsp;&nbsp;&nbsp;&nbsp;My name is Timothy Hatch! I am a Full Cycle Developer, Gamer, Golfer, Hockey player, Tech enthusiast.
+                I have been completing projects from inception to production for almost 9 years now.
+                I've worked across a variety of stacks and technologies. My favorite was developing motion code to move a 6-axis robot arm through our 3d world to help automate manufacturing.
+                In my career I have learned that there is so much to know and learn as a developer and I am never going to know all of it, but I'm always eager to expand my knowledge.
                 The things I have found to overcome the knowledge barrier are: always being ready to learn and knowing how to research to find the answer.
             </Box>
             <PdfViewer file="./hatch_resume_2024.pdf" />
